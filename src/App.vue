6815f792cb132036df69e3373d78823f2ec19a7e
Ilya Zhuravlev <ichirozcom@gmail.com>
2021-03-10 01:54:02 +0300

    feat(app): main menu

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container app">
     <div class="sidebar">
       <profile />
+      <main-menu class="main-menu" />
     </div>
     <div class="content">
       <router-view/>
@@ -10,10 +11,12 @@
 </template>
 
 <script>
-import Profile from "@/components/profile";
+import Profile from "@/components/Profile";
+import MainMenu from "@/components/MainMenu";
+
 export default {
   name: 'App',
-  components: {Profile},
+  components: {Profile, MainMenu},
 };
 </script>
 <style lang="scss" scoped>
@@ -24,9 +27,14 @@ export default {
     display: flex;
     flex: 1 0;
     flex-direction: column;
-    align-items: center;
+    align-items: flex-start;
     height: 100%;
     max-width: min(240px, 100%);
+
+    .main-menu {
+      margin-top: 75px;
+      margin-left: 72px;
+    }
   }
   .content {
     width: 100%;
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,86 @@
+<template>
+  <ul class="menu">
+    <li class="menu__item">
+      <router-link
+        class="menu__link"
+        to="/">
+        Home
+      </router-link>
+    </li>
+    <li class="menu__item">
+      <router-link
+        class="menu__link"
+        to="/events">
+        Events
+      </router-link>
+    </li>
+    <li class="menu__item">
+      <router-link
+        class="menu__link"
+        to="/results">
+        Results
+      </router-link>
+    </li>
+    <li class="menu__item">
+      <router-link
+        class="menu__link"
+        to="/tropaion">
+        Tropaion
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "MainMenu"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/config/_variables.scss";
+
+.menu {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+  &__item {
+    color: $text-inactive;
+
+    .menu__link {
+      position: relative;
+      font-weight: 700;
+      font-size: 1rem;
+      text-transform: uppercase;
+
+      &:after {
+        content: '';
+        display: inline-block;
+        visibility: hidden;
+        width: 0;
+        height: 3px;
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+        background-color: $text-primary;
+        transition: width 300ms ease-in;
+      }
+
+      &:hover,
+      &.is-active {
+        color: $text-primary;
+
+        &:after {
+          width: 100%;
+          visibility: visible;
+        }
+      }
+    }
+
+    & + .menu__item {
+      margin-top: 40px;
+    }
+  }
+}
+</style>
