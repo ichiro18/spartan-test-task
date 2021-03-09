6e51d3a94042decdf7fb53b2fed320b811f70b50
Ilya Zhuravlev <ichirozcom@gmail.com>
2021-03-09 19:05:53 +0300

    feat(app): profile component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
 <template>
-  <router-view />
+  <div class="container app">
+    <div class="sidebar">
+      <profile />
+    </div>
+    <div class="content">
+      <router-view/>
+    </div>
+  </div>
 </template>
 
 <script>
+import Profile from "@/components/profile";
 export default {
   name: 'App',
+  components: {Profile},
 };
 </script>
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  flex: 1 0;
+  .sidebar {
+    display: flex;
+    flex: 1 0;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    max-width: min(240px, 100%);
+  }
+  .content {
+    width: 100%;
+    flex: 1 0;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from "@/services/router/router";
 import store from "@/services/store/store";
-import '@/assets/index.css';
+import '@/assets/styles/theme.scss';
 
 Vue.config.productionTip = false;
 
